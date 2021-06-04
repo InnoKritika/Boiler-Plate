ac1e21a4b87f6b138d43a8c85e2b4ddf1b003a66
Kritika <knagwadia2907@gmail.com>
2021-06-04 13:29:21 +0530

    image picker

---
+++ app/src/main/java/com/example/boilerplate/Activities/BoilerPlate.java
@@ -0,0 +1,16 @@
+package com.example.boilerplate.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.boilerplate.R;
+
+public class BoilerPlate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.boiler_plate);
+    }
+}
